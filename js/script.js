68525b0863a31bbd619bcccb6e2ca36f94c9dffd
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-02-19 15:18:57 +0100

    [add] variables e prompt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+const name = prompt('Inserisci il nome');
+console.log('"Il nome è" ' + name);
 
+const lastName = prompt('Inserisci il cognome');
+console.log('"Il cognome è" ' + lastName);
+
+const color = prompt('Inserisci il tuo colore preferito');
+console.log('"Il colore preferito è" ' + color);
